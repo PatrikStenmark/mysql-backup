6061d91dcc292c51892c8a1299f94805cd7da3d8
Patrik Stenmark <patrik.stenmark@gmail.com>
2010-06-30 08:58:46 +0200

    remove backups from S3

--- mysql-backup.rb
+++ mysql-backup.rb
@@ -12,8 +12,8 @@ unless File.exists?(CONFIG[:mysql_config])
   exit 1
 end
 
-if File.stat(CONFIG[:mysql_config]).gid != 0 && (File.stat(CONFIG[:mysql_config]).mode & 0077) != 0
-  puts "Noooo, #{CONFIG[:mysql_config]} shall not be readable by any other than root"
+if (File.stat(CONFIG[:mysql_config]).mode & 0077) != 0
+  puts "#{CONFIG[:mysql_config]} shall not be world/group readable!"
   raise SystemExit
 end
 
@@ -42,21 +42,6 @@ CONFIG[:databases].each do |database|
   end
   debug("Cleaning up old backups for #{database}")
   
-  backups = Dir[File.join(target_dir, "*.bz2")].sort.reverse
-  debug("backups = #{backups.inspect}")
-  debug("keep = #{CONFIG[:keep_backups]}")
-  backups_to_delete = backups.slice!(CONFIG[:keep_backups].to_i, backups.size) || []
-  debug("to delete = #{backups_to_delete.inspect}")
-  
-  backups_to_delete.each do |backup_to_delete|
-    debug("Would delete #{backup_to_delete}")
-    File.delete(backup_to_delete)
-  end
-  filename = File.join(target_dir, "#{database}-#{Time.now.strftime("%Y%m%d%s")}.bz2")
-  debug("filename = #{filename}")
-  run("#{mysqldump} --defaults-file=#{CONFIG[:mysql_config]} #{database} | bzip2 > #{filename}")
-  #run("touch #{filename}")
-  
   if CONFIG[:s3]
     raise "S3 needs :s3_bucket, :s3_access_key_id and :s3_secret_access_key" unless CONFIG[:s3_secret_access_key] && CONFIG[:s3_access_key_id]
     s3 = Aws::S3.new(
@@ -64,9 +49,35 @@ CONFIG[:databases].each do |database|
       CONFIG[:s3_secret_access_key]
     )
     
+    bucket = s3.bucket(CONFIG[:s3_bucket])
+    
+    old_keys = bucket.keys(:prefix => database)
+    old_keys = old_keys.sort { |x, y| x.name <=> y.name }.reverse.slice(CONFIG[:keep_backups].to_i, old_keys.size) || []
+    
+    debug("will delete: #{old_keys.map(&:name).inspect}")
+    old_keys.each do |key|
+      key.delete
+    end
+  else
+    backups = Dir[File.join(target_dir, "*.bz2")].sort.reverse
+    debug("backups = #{backups.inspect}")
+    debug("keep = #{CONFIG[:keep_backups]}")
+    backups_to_delete = backups.slice!(CONFIG[:keep_backups].to_i, backups.size) || []
+    debug("to delete = #{backups_to_delete.inspect}")
+    
+    backups_to_delete.each do |backup_to_delete|
+      debug("Would delete #{backup_to_delete}")
+      File.delete(backup_to_delete)
+    end
+  end
+  filename = File.join(target_dir, "#{database}-#{Time.now.strftime("%Y%m%d-%H%M%S")}.bz2")
+  debug("filename = #{filename}")
+  run("#{mysqldump} --defaults-file=#{CONFIG[:mysql_config]} #{database} | bzip2 > #{filename}")
+  #run("touch #{filename}")
+  
+  if CONFIG[:s3]
     s3_key = File.basename(filename)
     debug("Storing #{s3_key} in S3")
-    bucket = s3.bucket(CONFIG[:s3_bucket])
     bucket.put(s3_key, open(filename))
     debug("Done storing #{filename} in S3")
     
